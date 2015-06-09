534e9eecbf9fa645ffee92c27325b1b16367a182
Zach Wise <zach@digitalartwork.net>
2015-06-09 08:20:01 -0500

    Update stories

--- stories/finding_closure/js/story.js
+++ stories/finding_closure/js/story.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
-	var vivus = new Vivus('houses', {type: 'oneByOne', duration: 100, file: 'img/houses.svg'}, onSVGFinish);
-
+	var vivus = new Vivus('houses', {type: 'oneByOne', duration: 125, file: 'img/houses.svg'}, onSVGFinish);
 	function onSVGFinish() {
-		console.log('finsihed');
+	
+		var childURL = 'http://duner.me/nu/medill/390/dist/index.html';
+		var pymParent = new pym.Parent('pym-parent', childURL, {});
 	}
+
 });
